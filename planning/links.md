136dad50f5b89f29a4c2390a54a4cd3dab698ec1
AndrewDHulse <theandrewhulse@gmail.com>
2023-07-10 23:25:14 -0500

    oAuth functioning

--- planning/links.md
+++ planning/links.md
@@ -5,3 +5,6 @@ Trello board https://trello.com/b/Fo08sRdd/project-2
 
 Wireframe https://excalidraw.com/
     
+
+RESTful routing reference sheet https://git.generalassemb.ly/SEIR-6-5-23/course-materials/blob/main/projects/project-2/rest-crud-chart.md 
+
---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const passport = require('passport');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//google oauth route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+  }
+));
+
+//google oauth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/movies');
+  });
+});
+
+module.exports = router;
