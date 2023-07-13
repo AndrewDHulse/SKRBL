f6b5f459fb3c6212e843b5247b8ef70546c58b2b
AndrewDHulse <theandrewhulse@gmail.com>
2023-07-13 17:07:14 -0500

    data.js is filled out, seed however is empty

---
+++ data.js
@@ -0,0 +1,18 @@
+exports.prompts- [
+    {
+    title: 'Eavesdropping',
+    content: 'Write about a time you overheard something you were not meant to hear'
+    },
+    {
+    title: 'Mind Reader',
+    content: ` You have woken up to find you can read minds, but you cannot 'turn it off' `
+    },
+    {
+    title: 'The Stranger' ,
+    content: 'Write about a time a chance encounter with a stranger left a lasting impression on you'
+    },
+    {
+    title: 'Hardships',
+    content: 'Write a poem to an aspect or element of the natural world'
+    }, 
+];
