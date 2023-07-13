042bb1b67191beb3c9412b9ed5961ce5583a5785
AndrewDHulse <theandrewhulse@gmail.com>
2023-07-13 17:39:24 -0500

    seed.js filled out

---
+++ seed.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+require('config/database');
+
+const Prompt = require('./models/prompt');
+const Post = require ('models/post');
+const data = require('./data');
+
+(async function() {
+    const p1 = Post.deleteMany({});
+    const p2 = Prompt.deleteMany({});
+    let results= await Promise.all([p1, p2]);
+    console.log(results);
+
+    results = await Promise.all([
+        Prompt.create(data.prompt),
+]);
+console.log('created prompts:', results[0]);
+
+process.exit();
+})();
